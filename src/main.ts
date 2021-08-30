27c9c97f12fad700a8cd6ed40b52bf65c851e704
Emmanuel Walujo <emmanuel.walujo@technobraingroup.com>
2021-08-30 12:22:02 +0300

    close and start again in watch mode

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.close();
   await app.listen(8007);
   console.log('ARJS-NESTJS-API running on port 8007...');
 }
